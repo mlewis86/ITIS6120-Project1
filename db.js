54aa1cb9a124b976908ca752e79a84f06ea7e8f1
ErikaSudderth <erikasudderth@gmail.com>
2024-12-01 12:46:22 -0500

    Change dashboards

--- db.js
+++ db.js
@@ -1,30 +1,28 @@
 // db.js
-const mysql = require('mysql2');
-const mongoose = require('mongoose');
+const mysql = require("mysql2");
+const mongoose = require("mongoose");
 
 // MySQL Connection
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '2627Hb##',
-  database: 'Emergency_Department',
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "Emergency_Department",
   port: 3306
 });
 
-
 // MongoDB Connection for User Authentication
 const connectDB = async () => {
-        try {
-            await mongoose.connect('mongodb://localhost:27017/userAuthentication', {
-              
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            });
-            console.log('Connected to MongoDB');
-        } catch (error) {
-            console.error('Error connecting to MongoDB:', error.message);
-            process.exit(1);
-        }
-    };
+  try {
+    await mongoose.connect("mongodb://localhost:27017/userAuthentication", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = { mysqlConnection, mongoose, connectDB };
